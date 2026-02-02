4d890e8c21100788773fe0420b3734fdbe6906c0
uriHenig <uryhenig@gmail.com>
2026-02-02 21:38:10 +0200

    Add animation to clicked button

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,13 +1,6 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  gamePattern.push(buttonColours[randomNumber]);
-  var randomChosenColour = gamePattern.at(-1);
-  buttonAnimation(randomChosenColour);
-  playSound(randomChosenColour);
-}
 
 function buttonAnimation(randomColor) {
   $("div#" + randomColor).addClass("hidden");
@@ -20,10 +13,26 @@ function playSound(randomColor) {
   sound.play();
 }
 
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  gamePattern.push(buttonColours[randomNumber]);
+  var randomChosenColour = gamePattern.at(-1);
+  buttonAnimation(randomChosenColour);
+  playSound(randomChosenColour);
+}
+
+function animateClick(currentColour) {
+  $("div#" + currentColour).addClass("pressed");
+  setTimeout(() => {
+    $("div#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
 // store clicked color in array and play corresponding sound
 $("div.btn").on("click", function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+  animateClick(userChosenColour);
   playSound(userChosenColour);
   console.log(userClickedPattern);
 });
