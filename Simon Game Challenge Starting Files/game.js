26bb3b56c2ab07f1010287529da711d40f410ce9
uriHenig <uryhenig@gmail.com>
2026-02-06 06:15:43 +0200

    Add main logic of game
    - function checkAnswer() - check user sequenc vs. game sequence

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -4,20 +4,61 @@ var userClickedPattern = [];
 var gameStarted = false;
 var level = 0;
 
+// 1. initiates game
 $(document).on("keydown", function () {
-  console.log("first");
-  console.log(gameStarted);
   if (!gameStarted) {
     nextSequence();
     gameStarted = true;
-    console.log(gameStarted);
   } else {
     return;
   }
 });
 
+// 2. changes level + generates random color + stores sequence of colors
+function nextSequence() {
+  level++;
+  changeLevels(level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  gamePattern.push(buttonColours[randomNumber]);
+  var randomChosenColour = gamePattern.at(-1);
+  buttonAnimation(randomChosenColour);
+  playSound(randomChosenColour);
+  console.log(gamePattern);
+}
+
+// 3. user clicks + userClickedPattern is checked against gamePattern
+$("div.btn").on("click", function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  animateClick(userChosenColour);
+  playSound(userChosenColour);
+  console.log(userClickedPattern);
+  checkAnswer(userClickedPattern);
+}); //TODO currently, checkAnswer is called on every click,change to check only once user has finished clicking
+
+function checkAnswer(currentLevel) {
+  var i = currentLevel.length - 1;
+  if (currentLevel[i] === gamePattern[i]) {
+    console.log("success");
+    if (currentLevel.length === gamePattern.length) {
+      //check only when user has finished clicking a
+      setTimeout(() => {
+        nextSequence();
+        userClickedPattern = [];
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+  }
+}
+
 function changeLevels(level) {
   $("h1").text("Level " + level);
+}
+
+function playSound(randomColor) {
+  const sound = new Audio("sounds/" + randomColor + ".mp3");
+  sound.play();
 }
 
 function buttonAnimation(randomColor) {
@@ -26,21 +67,6 @@ function buttonAnimation(randomColor) {
     $("div#" + randomColor).removeClass("hidden");
   }, 100);
 }
-function playSound(randomColor) {
-  const sound = new Audio("sounds/" + randomColor + ".mp3");
-  sound.play();
-}
-
-function nextSequence() {
-  console.log("second");
-  var randomNumber = Math.floor(Math.random() * 4);
-  gamePattern.push(buttonColours[randomNumber]);
-  var randomChosenColour = gamePattern.at(-1);
-  buttonAnimation(randomChosenColour);
-  playSound(randomChosenColour);
-  level++;
-  changeLevels(level);
-}
 
 function animateClick(currentColour) {
   $("div#" + currentColour).addClass("pressed");
@@ -48,13 +74,4 @@ function animateClick(currentColour) {
     $("div#" + currentColour).removeClass("pressed");
   }, 100);
 }
-
-// store clicked color in array and play corresponding sound
-
-$("div.btn").on("click", function () {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-  animateClick(userChosenColour);
-  playSound(userChosenColour);
-  console.log(userClickedPattern);
-});
+// when user clicks, check the index of answer to gameSequence
