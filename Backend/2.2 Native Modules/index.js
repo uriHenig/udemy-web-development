23a823e4a5f9f60278160f0579be79fc04e4ef63
uriHenig <uryhenig@gmail.com>
2026-02-10 07:00:21 +0200

    Complete lessons 2.1 and 2.2

---
+++ Backend/2.2 Native Modules/index.js
@@ -0,0 +1,10 @@
+const { ifError } = require("assert");
+const fs = require("fs");
+// fs.writeFile("message.txt", "hello from nodeJS", (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// });
+fs.readFile("./message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
